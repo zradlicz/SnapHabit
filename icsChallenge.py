56e84feca5280ea62e8cc5b90c794892f320e089
zradlicz <66800917+zradlicz@users.noreply.github.com>
2020-06-12 13:08:26 -0500

    Add files via upload

--- icsChallenge.py
+++ icsChallenge.py
@@ -4,13 +4,25 @@ from datetime import datetime
 import arrow
 
     
-def ChangeStartDate(old,new,oldfile,newfile): #function takes old startdate (old), and new startdate (new), old calendar ics file, and the new calendar ics file.
-    filename = oldfile
-    file = open(filename,'r',encoding='utf8')
-    c = Calendar(file.read())    
+def findStart (cal):
+    start = arrow.get('9999-12-30')
+    for event in cal.events:
+        date = event.begin
+        if date < start:
+            start = date
+    return start
     
-    startdate = arrow.get(new) #convert to datetime/arrow object
-    firstdate = arrow.get(old)
+
+
+def changeStartDate(file,date): #function takes old startdate (old), and new startdate (new), old calendar ics file, and the new calendar ics file.
+    filename = file
+    CalFile = open(filename,'r',encoding='utf8')
+    c = Calendar(CalFile.read())    
+    
+    
+    
+    startdate = arrow.get(date) #convert to datetime/arrow object
+    firstdate = findStart(c)
     delta = startdate-firstdate #the amount of time between old start date and new start date
     
     for event in c.events: #iterates through every event in the calendar and changes the start and end date by 'delta' amount of time
@@ -22,11 +34,11 @@ def ChangeStartDate(old,new,oldfile,newfile): #function takes old startdate (old
         print(s.datetime)
         print(e.datetime)
 
-    with open(newfile,'w') as f: #writes to new file
+    with open('Edited'+file,'w') as f: #writes to new file
         f.writelines(c)
         
-ChangeStartDate('2020-06-04','2020-06-15','JusticeJune.ics','JusticeJuneJumped.ics')
-    
+changeStartDate('JusticeJune.ics','2020-06-15')
+   
         
 
 
---
+++ icsREADME.txt
@@ -0,0 +1,10 @@
+###README###
+
+The program contains two functions. 
+The main funtion is changeStartDate. It takes a file name and a date as arguments.
+The date is the new start date for the first event on the calendar. 
+The last line in the program is where the file and date are inputed.
+The program must be in the same directory that the file is located in.
+
+changeStartDate will shift all the events in the calendar over according to the new start date.
+
